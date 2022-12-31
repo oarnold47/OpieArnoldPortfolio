46a6901841bca1cb9ce5a372b379651f7f21eff2
oarnold47 <raymondbusfree@gmail.com>
2022-12-31 00:09:57 -0800

    too many updates
    to count

--- js/main.js
+++ js/main.js
@@ -1,29 +1,45 @@
-/* document.addEventListener("DOMContentLoaded", function() {
-    fields.firstName = document.getElementById('name');
-    fields.email = document.getElementById('email');
-    fields.subject = document.getElementById('subject');
-    fields.comment = document.getElementById('comment');
-   })
+const form = document.getElementById("form");
+const result = document.getElementById("result");
 
-   function isNotEmpty(value) {
-    if (value == null || typeof value == 'undefined' ) return false;
-    return (value.length > 0);
-   }
+form.addEventListener("submit", function (e) {
+  const formData = new FormData(form);
+  e.preventDefault();
+  var object = {};
+  formData.forEach((value, key) => {
+    object[key] = value;
+  });
+  var json = JSON.stringify(object);
+  result.innerHTML = "Please wait...";
 
-   function isEmail(email) {
-    let regex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
-    return regex.test(String(email).toLowerCase());
-   }
-
-   function isValid() {
-    var valid = true;
-    
-    valid &= fieldValidation(fields.name, isNotEmpty);
-    valid &= fieldValidation(fields.email, isEmail);
-    valid &= fieldValidation(fields.subject, isNotEmpty);
-    valid &= fieldValidation(fields.comment, isNotEmpty);
-    valid &= arePasswordsEqual();
-   
-    return valid;
-   }
-*/
+  fetch("https://api.web3forms.com/submit", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json"
+    },
+    body: json
+  })
+    .then(async (response) => {
+      let json = await response.json();
+      if (response.status == 200) {
+        result.innerHTML = json.message;
+        result.classList.remove("text-gray-500");
+        result.classList.add("text-green-500");
+      } else {
+        console.log(response);
+        result.innerHTML = json.message;
+        result.classList.remove("text-gray-500");
+        result.classList.add("text-red-500");
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      result.innerHTML = "Something went wrong!";
+    })
+    .then(function () {
+      form.reset();
+      setTimeout(() => {
+        result.style.display = "none";
+      }, 5000);
+    });
+});

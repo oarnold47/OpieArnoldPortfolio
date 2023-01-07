f554c07e7f9bc4fe6ea7a0b56ac5f42bfa6f802f
oarnold47 <raymondbusfree@gmail.com>
2023-01-07 11:28:58 -0800

    so many updates
    check it out

--- js/main.js
+++ js/main.js
@@ -43,3 +43,82 @@ form.addEventListener("submit", function (e) {
       }, 5000);
     });
 });
+
+/*var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("menu").style.top = "0";
+  } else {
+    document.getElementById("menu").style.top = "-100px";
+  }
+ prevScrollpos = currentScrollPos;
+}*/
+
+/*document.addEventListener("DOMContentLoaded", function(){
+
+  el_autohide = document.querySelector('.autohide');
+
+  navbar_height = document.querySelector('.navbar').offsetHeight;
+  document.body.style.paddingTop = navbar_height + 'px';
+
+  if(el_autohide){
+    var last_scroll_top = 0;
+    window.addEventListener('scroll', function() {
+          let scroll_top = window.scrollY;
+         if(scroll_top < last_scroll_top) {
+              el_autohide.classList.remove('scrolled-down');
+              el_autohide.classList.add('scrolled-up');
+          }
+          else {
+              el_autohide.classList.remove('scrolled-up');
+              el_autohide.classList.add('scrolled-down');
+          }
+          last_scroll_top = scroll_top;
+    }); 
+    // window.addEventListener
+  }
+  // if
+
+}); */
+
+var lastScrollTop;
+
+navbar = document.getElementById('navbar');
+
+window.addEventListener('scroll',function(){
+  var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    if(scrollTop > lastScrollTop){
+      navbar.style.top='-100px';
+    }
+    else{
+    navbar.style.top='0';
+    }
+  lastScrollTop = scrollTop;
+});
+
+/*$(document).ready(function() {
+  onScroll();
+});
+
+function onScroll() {   
+  var lastScrollTop, $bar = $('#navbar') , height = $bar.height(), 
+      $window = $(window), offset = 10;
+
+  $window.on('scroll',function()  {       
+      var scrollTop = $window.scrollTop();
+      $bar.css({ top: scrollTop > lastScrollTop && scrollTop > offset ? -height : 0 });
+      lastScrollTop = scrollTop; 
+  });     
+}*/
+
+
+$(document).ready(function () {
+    $(document).click(function (event) {
+        var clickover = $(event.target);
+        var _opened = $(".collapse").hasClass("in");
+        if (!$(event.target).closest('.navbar').length && _opened === true && !clickover.hasClass("navbar-toggle")) {
+            $(".collapse").collapse('toggle');
+        }
+    });
+});
